5243464e19aa733ea09c3cd044144f042d68fe6a
IlMelons <melonss2002@gmail.com>
2025-07-16 21:10:16 +0200

    Added autoshuffle/radio disabler

--- client/client.lua
+++ client/client.lua
@@ -1,7 +1,21 @@
 local blacklist = lib.load("config.blacklist")
 
----@type blacklist table <model, hash> <toggle, boolean>
----@description Vehicle Model Suppressor (Avoids vehicles spawning in traffic)
 for model, toggle in ipairs(blacklist) do
     SetVehicleModelIsSuppressed(model, toggle)
 end
+
+lib.onCache("seat", function(seat)
+    if seat == -1 and cache.vehicle then
+        SetUserRadioControlEnabled(false)
+
+        if GetPlayerRadioStationIndex() ~= 255 then
+            SetVehRadioStation(cache.vehicle, "OFF")
+        end
+    end
+
+    SetPedConfigFlag(cache.ped, 184, true)
+
+    if cache.vehicle and not cache.seat then
+        SetPedIntoVehicle(cache.ped, cache.vehicle, seat)
+    end
+end)
